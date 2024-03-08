b1ae8bb1d1dbb7102e44dc74da8b7d75a3ad0f97
Azziq Shamsudin <muhammadazziqshamsu@airasia.com>
2024-03-09 06:41:34 +0800

    update again3

--- dist/models/responseModel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.responseModel = void 0;
-class responseModel {
-}
-exports.responseModel = responseModel;
-//# sourceMappingURL=responseModel.js.map
